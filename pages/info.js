72c2e153df79f73080ecb2004de377d8fa8c768c
Parker <pp101socials@gmail.com>
2023-06-07 12:28:51 -0400

    Added components

--- pages/info.js
+++ pages/info.js
@@ -5,9 +5,13 @@ import { useRouter } from "next/router";
 // Library Imports
 
 // Data/Functions/Images Imports
+import ManipPageLink from "@/assets/functions/dom/manip/ManipPageLinks";
 
 // Component Imports
 import { PageHead } from "@/assets/components/global/All/PageHead";
+import { DesktopNav } from "@/assets/components/global/Nav/Desktop/DesktopNav";
+import { MobileNav } from "@/assets/components/global/Nav/Mobile/MobileNav";
+import { MobileNavMenu } from "@/assets/components/global/Nav/Mobile/MobileNavMenu";
 
 // Style Imports
 import "../assets/styles/modules/Info/Info.module.css";
@@ -15,11 +19,21 @@ import "../assets/styles/modules/Info/Info.module.css";
 export default function Info() {
   const router = useRouter();
 
+  // Disabling Page Links
+  useEffect(() => {
+    ManipPageLink("disable", "multiple", ".info-link");
+  }, []);
+
   return (
     <div id="PAGE" className="page overrides_Info full-second">
       <PageHead />
 
-      <main id="PAGE_CNT"></main>
+      <MobileNavMenu />
+
+      <main id="PAGE_CNT">
+        <DesktopNav />
+        <MobileNav />
+      </main>
     </div>
   );
 }

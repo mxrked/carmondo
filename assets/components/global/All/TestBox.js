f568da938b37d82a5efc237a643f87c7ca981048
Parker <pp101socials@gmail.com>
2023-06-07 13:37:47 -0400

    Created file

---
+++ assets/components/global/All/TestBox.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *  This is a test box
+ *
+ */
+
+import styles from "../../../styles/modules/Test/Test.module.css";
+
+export const TestBox = () => {
+  return <div className={`${styles.test_box}`} />;
+};

03efbf1ed67672fdd9da4331102c584c12755196
Parker <pp101socials@gmail.com>
2023-06-11 18:23:01 -0400

    Added jsons

--- assets/cdns/CDNFiles.js
+++ assets/cdns/CDNFiles.js
@@ -6,4 +6,20 @@
 
 import { CDNFileReturn } from "./CDNReturns";
 
-export {};
+const JSON_ALL_VEHICLES = CDNFileReturn("json", "ALL_VEHICLES", "json");
+const JSON_INDEX_VEHICLES = CDNFileReturn("json", "INDEX_VEHICLES", "json");
+const JSON_COUPES = CDNFileReturn("json", "COUPES", "json");
+const JSON_PICKUP_TRUCKS = CDNFileReturn("json", "PICKUP_TRUCKS", "json");
+const JSON_SEDANS = CDNFileReturn("json", "SEDANS", "json");
+const JSON_SUVS = CDNFileReturn("json", "SUVS", "json");
+const JSON_WAGONS = CDNFileReturn("json", "WAGONS", "json");
+
+export {
+  JSON_ALL_VEHICLES,
+  JSON_COUPES,
+  JSON_INDEX_VEHICLES,
+  JSON_PICKUP_TRUCKS,
+  JSON_SEDANS,
+  JSON_SUVS,
+  JSON_WAGONS,
+};

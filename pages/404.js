e1032c2b71f542940a1b707b4cdde6acd26a2e1d
Parker <pp101socials@gmail.com>
2023-06-12 10:21:23 -0400

    Added components

--- pages/404.js
+++ pages/404.js
@@ -5,9 +5,12 @@ import { useRouter } from "next/router";
 // Library Imports
 
 // Data/Functions/Images Imports
+import { TriggerExitAnimations } from "@/assets/functions/dom/triggers/TriggerExitAnimations";
 
 // Component Imports
 import { PageHead } from "@/assets/components/global/All/PageHead";
+import { NotFoundLogo } from "@/assets/components/pages/NotFound/NotFoundLogo";
+import { NotFoundMain } from "@/assets/components/pages/NotFound/NotFoundMain";
 
 // Style Imports
 import "../assets/styles/modules/NotFound/NotFound.module.css";
@@ -15,11 +18,19 @@ import "../assets/styles/modules/NotFound/NotFound.module.css";
 export default function NotFound() {
   const router = useRouter();
 
+  // Triggering exit animations
+  useEffect(() => {
+    TriggerExitAnimations();
+  }, []);
+
   return (
     <div id="PAGE" className="page overrides_404 full-second">
       <PageHead />
 
-      <main id="PAGE_CNT"></main>
+      <main id="PAGE_CNT">
+        <NotFoundLogo />
+        <NotFoundMain />
+      </main>
     </div>
   );
 }

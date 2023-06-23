9f25b01fb351e13abdf8881bba4fbbd94cbe7b63
Parker <pp101socials@gmail.com>
2023-06-23 07:55:54 -0400

    Created file

---
+++ assets/data/classes/Type.js
@@ -0,0 +1,8 @@
+export default class Type {
+  constructor(typeID, typeName, typeBg, typeLink) {
+    this._typeID = typeID;
+    this._typeName = typeName;
+    this._typeBg = typeBg;
+    this._typeLink = typeLink;
+  }
+}
